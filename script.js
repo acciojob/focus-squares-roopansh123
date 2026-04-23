2ae65c79c17ef6084608d67899191252bbd500cf
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2026-04-23 11:57:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        squares.forEach((sq) => {
+            if (sq !== square) {
+                sq.style.backgroundColor = "#6F4E37"; 
+            }
+        });
+    });
+
+    square.addEventListener("mouseout", () => {
+        squares.forEach((sq) => {
+            sq.style.backgroundColor = "#E6E6FA"; });
+    });
+});
